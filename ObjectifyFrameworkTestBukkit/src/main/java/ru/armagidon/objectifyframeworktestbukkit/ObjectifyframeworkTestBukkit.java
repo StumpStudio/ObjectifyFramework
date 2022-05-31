d87be995987371c6e32b74642c5d5b692a334e47
Armagidon <yt.armagidon.mc@gmail.com>
2022-05-31 22:44:08 +0300

    Project init

---
+++ ObjectifyFrameworkTestBukkit/src/main/java/ru/armagidon/objectifyframeworktestbukkit/ObjectifyframeworkTestBukkit.java
@@ -0,0 +1,17 @@
+package ru.armagidon.objectifyframeworktestbukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ObjectifyframeworkTestBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
